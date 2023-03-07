8731afbdea52ffa4085ecfc1978115aaaaa13eea
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-03-06 21:41:07 -0500

    Fix 1 eslint:all and react:all error
    10 remain to be fixed.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,19 +3,44 @@ import React from "react";
 
 export type CardProps = {
   dealtCard: DealtCard;
-  toggleKept: () => void;
+  dealOrderIndex: number;
+  toggleKept: (index: number) => void;
 };
 
-export function Card({
-  dealtCard: { kept, rankLabel },
-  toggleKept,
-}: CardProps) {
-  return (
-    <li
-      className={`card${kept ? "" : " discarded"}`}
-      onClick={toggleKept}
-    >
-      {rankLabel}
-    </li>
-  );
+export class Card extends React.Component<CardProps> {
+  constructor(props: CardProps) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  override shouldComponentUpdate(nextProps: CardProps) {
+    const {
+      dealOrderIndex,
+      dealtCard: { index, kept },
+    } = this.props;
+    return (
+      dealOrderIndex !== nextProps.dealOrderIndex ||
+      index !== nextProps.dealtCard.index ||
+      kept !== nextProps.dealtCard.kept
+    );
+  }
+
+  handleClick() {
+    const { dealOrderIndex, toggleKept } = this.props;
+    toggleKept(dealOrderIndex);
+  }
+
+  override render() {
+    const {
+      dealtCard: { kept, rankLabel },
+    } = this.props;
+    return (
+      <li
+        className={`card${kept ? "" : " discarded"}`}
+        onClick={this.handleClick}
+      >
+        {rankLabel}
+      </li>
+    );
+  }
 }
