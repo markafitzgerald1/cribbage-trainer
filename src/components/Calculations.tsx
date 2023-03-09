dadf7adb050e60bbf34ecbf5bc6ac83df5aff82c
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-03-08 20:16:57 -0500

    Simplify runs points calculations

--- src/components/Calculations.tsx
+++ src/components/Calculations.tsx
@@ -4,13 +4,6 @@ import { Calculation } from "./Calculation";
 import { DealtCard } from "../DealtCard";
 import React from "react";
 
-const POINTS = {
-  FIFTEENS: 2,
-  FOUR_CARD_RUN: 4,
-  PAIR: 2,
-  THREE_CARD_RUN: 3,
-} as const;
-
 const COUNT = {
   FIFTEEN: 15,
 } as const;
@@ -22,19 +15,20 @@ function getAllKeepDiscardCombinations(dealtCards: DealtCard[]) {
   }));
 }
 
-const CARD_COUNTS = {
-  FIFTEEN: 2,
+const PLAY_POINTS = {
+  FIFTEEN_COUNT: 2,
   FOUR_RUN: 4,
   PAIR: 2,
+  RUN_PER_CARD: 1,
   THREE_RUN: 3,
 } as const;
 
 const pairsPoints = (keep: DealtCard[]) =>
-  [...new Combination(keep, CARD_COUNTS.PAIR)].filter(
+  [...new Combination(keep, PLAY_POINTS.PAIR)].filter(
     (possiblePair) =>
       (possiblePair[0] as DealtCard).rankValue ===
       (possiblePair[1] as DealtCard).rankValue
-  ).length * POINTS.PAIR;
+  ).length * PLAY_POINTS.PAIR;
 
 const fifteensPoints = (keep: DealtCard[]) =>
   [...new PowerSet(keep)].filter(
@@ -42,48 +36,39 @@ const fifteensPoints = (keep: DealtCard[]) =>
       possibleFifteen
         .map((card) => card.count)
         .reduce((count1, count2) => count1 + count2, 0) === COUNT.FIFTEEN
-  ).length * POINTS.FIFTEENS;
+  ).length * PLAY_POINTS.FIFTEEN_COUNT;
 
-// TODO: zip with itself better: https://stackoverflow.com/questions/22015684/zip-arrays-in-javascript
-// e.g. a=[2,3,4]; a.slice(1).map((e, i) => e - a[i]).every((diff) => diff === 1) // true
-const threeRunPoints = (keep: DealtCard[]) =>
-  [...new Combination(keep, CARD_COUNTS.THREE_RUN)]
-    .map((possibleThreeRun) => {
-      possibleThreeRun.sort(
-        (card1, card2) => card1.rankValue - card2.rankValue
-      );
-      return possibleThreeRun;
-    })
-    .filter(
-      (possibleThreeRun) =>
-        (possibleThreeRun[0] as DealtCard).rankValue + 1 ===
-          (possibleThreeRun[1] as DealtCard).rankValue &&
-        (possibleThreeRun[1] as DealtCard).rankValue + 1 ===
-          (possibleThreeRun[2] as DealtCard).rankValue
-    ).length * POINTS.THREE_CARD_RUN;
+const RUN_LENGTHS = {
+  FIVE: 5,
+  FOUR: 4,
+  THREE: 3,
+} as const;
 
-// TODO: zip with itself better: https://stackoverflow.com/questions/22015684/zip-arrays-in-javascript
-// TODO: factor out common code with threeRunPoints if it exists post-zip().
-const fourRunPoints = (keep: DealtCard[]) =>
-  [...new Combination(keep, CARD_COUNTS.FOUR_RUN)]
-    .map((possibleFourRun) => {
-      possibleFourRun.sort((card1, card2) => card1.rankValue - card2.rankValue);
-      return possibleFourRun;
-    })
-    .filter(
-      (possibleFourRun) =>
-        (possibleFourRun[0] as DealtCard).rankValue + 1 ===
-          (possibleFourRun[1] as DealtCard).rankValue &&
-        (possibleFourRun[1] as DealtCard).rankValue + 1 ===
-          (possibleFourRun[2] as DealtCard).rankValue &&
-        (possibleFourRun[2] as DealtCard).rankValue + 1 ===
-          (possibleFourRun[3] as DealtCard).rankValue
-    ).length * POINTS.FOUR_CARD_RUN;
+enum RunLength {
+  THREE = RUN_LENGTHS.THREE,
+  FOUR = RUN_LENGTHS.FOUR,
+  FIVE = RUN_LENGTHS.FIVE,
+}
+
+const runPoints = (keep: DealtCard[], runLength: RunLength) =>
+  [...new Combination(keep, runLength)]
+    .map((combination) => combination.map((card) => card.rankValue))
+    .map((combination) =>
+      [...combination].sort((rank1, rank2) => rank1 - rank2)
+    )
+    .filter((combination) =>
+      combination
+        .slice(1)
+        .map((rank, index) => rank - (combination[index] as number))
+        .every((diff) => diff === 1)
+    ).length *
+  PLAY_POINTS.RUN_PER_CARD *
+  runLength;
 
 const countPoints = (keep: DealtCard[]) =>
   pairsPoints(keep) +
   fifteensPoints(keep) +
-  (fourRunPoints(keep) || threeRunPoints(keep));
+  (runPoints(keep, RunLength.FOUR) || runPoints(keep, RunLength.THREE));
 
 export function Calculations({ dealtCards }: { dealtCards: DealtCard[] }) {
   return (
