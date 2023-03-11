2c42ac49598cf131783e08148272e7ffe207a2c1
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-03-11 15:36:21 -0500

    Add ESLint Jest plugin

--- src/Scoring.test.ts
+++ src/Scoring.test.ts
@@ -1,16 +1,14 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import { pairsPoints } from "./Scoring";
 
-describe("Scoring", () => {
-  describe("pairsPoints", () => {
-    test("two equal rank cards", () => {
-      const expectedPoints = 2;
-      const QUEEN = {
-        count: 10,
-        rankLabel: "Q",
-        rankValue: 11,
-      };
-      expect(pairsPoints([QUEEN, QUEEN])).toBe(expectedPoints);
-    });
+describe("pairsPoints", () => {
+  it("two equal rank cards", () => {
+    const expectedPoints = 2;
+    const QUEEN = {
+      count: 10,
+      rankLabel: "Q",
+      rankValue: 11,
+    };
+    expect(pairsPoints([QUEEN, QUEEN])).toBe(expectedPoints);
   });
 });
