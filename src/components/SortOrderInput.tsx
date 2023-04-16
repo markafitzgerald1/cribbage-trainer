33b4541dd1f6224f3e87ec750391a09ae407de4f
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-04-16 19:31:15 -0400

    Factor out duplicate code
    and increase minimum required Jest test coverage levels to newly
    increased current coverage levels.

--- src/components/SortOrderInput.tsx
+++ src/components/SortOrderInput.tsx
@@ -1,6 +1,6 @@
+import { SortOrderName, sortOrderNames } from "../SortOrderName";
 import React from "react";
 import { SortOrder } from "../SortOrder";
-import { SortOrderName } from "../SortOrderName";
 
 interface SortOrderInputProps {
   sortOrder: SortOrder;
@@ -25,22 +25,19 @@ export class SortOrderInput extends React.Component<SortOrderInputProps> {
     return (
       <div className="sort-order">
         <span>Sort: </span>
-        {Object.keys(SortOrder)
-          .filter((key) => isNaN(Number(key)))
-          .map((key) => key as SortOrderName)
-          .map((key) => (
-            <span key={SortOrder[key]}>
-              <input
-                checked={sortOrder === SortOrder[key]}
-                id={key}
-                name="sort"
-                onChange={this.handleChange}
-                type="radio"
-                value={SortOrder[SortOrder[key]]}
-              />
-              <label htmlFor={key}>{SortOrderInput.SortLabel[key]}</label>
-            </span>
-          ))}
+        {sortOrderNames.map((key) => (
+          <span key={SortOrder[key]}>
+            <input
+              checked={sortOrder === SortOrder[key]}
+              id={key}
+              name="sort"
+              onChange={this.handleChange}
+              type="radio"
+              value={SortOrder[SortOrder[key]]}
+            />
+            <label htmlFor={key}>{SortOrderInput.SortLabel[key]}</label>
+          </span>
+        ))}
         <span className="sort-order-description">
           {" "}
           (
