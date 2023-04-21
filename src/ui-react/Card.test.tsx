6df80af32bfd553c515981d539b474fd1f5f5e92
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-04-20 22:47:17 -0400

    Add second Card component test

--- src/ui-react/Card.test.tsx
+++ src/ui-react/Card.test.tsx
@@ -2,24 +2,36 @@ import { describe, expect, it, jest } from "@jest/globals";
 import { Card } from "./Card";
 import { DealtCard } from "../game/DealtCard";
 import React from "react";
-import { createCard } from "../game/Card";
+import { dealHand } from "../game/dealHand";
 import { render } from "@testing-library/react";
 
 describe("card", () => {
-  it("returns a list item", () => {
-    const dealtCards: readonly DealtCard[] = [
-      { ...createCard(0), dealOrder: 0, kept: false },
-    ];
+  const createAndRenderCard = () => {
+    const dealtCards: readonly DealtCard[] = dealHand();
     const setDealtCards = jest.fn();
-
-    const { queryByRole } = render(
+    const dealOrderIndex = Math.floor(Math.random() * dealtCards.length);
+    const { getByRole, queryByRole } = render(
       <Card
-        dealOrderIndex={0}
+        dealOrderIndex={dealOrderIndex}
         dealtCards={dealtCards}
         setDealtCards={setDealtCards}
       />
     );
 
+    return { dealOrderIndex, dealtCards, getByRole, queryByRole };
+  };
+
+  it("returns a list item", () => {
+    const { queryByRole } = createAndRenderCard();
+
     expect(queryByRole("listitem")).toBeTruthy();
+  });
+
+  it("text is its rank label", () => {
+    const { dealOrderIndex, getByRole, dealtCards } = createAndRenderCard();
+
+    expect(getByRole("listitem").textContent).toBe(
+      dealtCards[dealOrderIndex]!.rankLabel
+    );
   });
 });
