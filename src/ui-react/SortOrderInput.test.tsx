8ea24b371feb59b8d667e3e7568313e33517f400
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-04-23 10:41:26 -0400

    Test SortOrderInput radio button count

--- src/ui-react/SortOrderInput.test.tsx
+++ src/ui-react/SortOrderInput.test.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { SortOrder } from "../ui/SortOrder";
 import { SortOrderInput } from "./SortOrderInput";
 import { render } from "@testing-library/react";
+import { sortOrderNames } from "../ui/SortOrderName";
 
 describe("sort order input component", () => {
   it("contains a group", () => {
@@ -14,5 +15,16 @@ describe("sort order input component", () => {
     );
 
     expect(queryByRole("group")).toBeTruthy();
+  });
+
+  it("contains a radio button for each sort order", () => {
+    const { queryAllByRole } = render(
+      <SortOrderInput
+        setSortOrder={jest.fn()}
+        sortOrder={SortOrder.Ascending}
+      />
+    );
+
+    expect(queryAllByRole("radio")).toHaveLength(sortOrderNames.length);
   });
 });
