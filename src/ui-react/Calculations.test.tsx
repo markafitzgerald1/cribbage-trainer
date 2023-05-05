3e8209b7731b5b010e4d4ea3746dae96446c000e
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-05-05 09:03:35 -0400

    DOM Test Calculations component functionality
    Also updated outdated dependency to latest.

---
+++ src/ui-react/Calculations.test.tsx
@@ -0,0 +1,55 @@
+import { describe, expect, it } from "@jest/globals";
+import { CARDS_PER_DISCARD } from "../game/facts";
+import { Calculations } from "./Calculations";
+import { Combination } from "js-combinatorics";
+import React from "react";
+import { SortOrder } from "../ui/SortOrder";
+import { allScoredKeepDiscardsByScoreDescending } from "../analysis/analysis";
+import { dealHand } from "../game/dealHand";
+import { handToSortedString } from "../ui/handToSortedString";
+import { render } from "@testing-library/react";
+
+describe("calculations component", () => {
+  const dealAndRender = () => {
+    const dealtHand = dealHand();
+
+    const { container } = render(
+      <Calculations
+        dealtCards={dealtHand}
+        sortOrder={SortOrder.Ascending}
+      />
+    );
+
+    return { container, dealtHand };
+  };
+
+  it("should render each possible keep and discard pair exactly once", () => {
+    const { container } = dealAndRender();
+
+    const nCombs = Number(
+      new Combination(dealHand(), CARDS_PER_DISCARD).length
+    );
+    const handPartsPerDeal = 2;
+    expect(container.querySelectorAll(".keep-discard")).toHaveLength(
+      nCombs * handPartsPerDeal
+    );
+  });
+
+  it("should contain each possible scored keep and discard", () => {
+    const { dealtHand, container } = dealAndRender();
+
+    allScoredKeepDiscardsByScoreDescending(dealtHand).forEach(
+      (scoredKeepDiscard) => {
+        expect(container.textContent).toContain(
+          `${handToSortedString(
+            scoredKeepDiscard.keep,
+            SortOrder.Ascending
+          )}-${handToSortedString(
+            scoredKeepDiscard.discard,
+            SortOrder.Ascending
+          )} for ${scoredKeepDiscard.points} points`
+        );
+      }
+    );
+  });
+});
