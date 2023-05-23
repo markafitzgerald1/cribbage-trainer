8f8b60ca3d783f09e19ef0a3cf765b3064dddc35
Mark Fitzgerald <mark.a.fitzgerald1@gmail.com>
2023-05-22 23:37:03 -0400

    Test general app happy path in index.html
    Ensures that index.html functions as intended from a React app in DOM
    container initialization and core functionality point of view.

--- tests-e2e/index.spec.ts
+++ tests-e2e/index.spec.ts
@@ -1,6 +1,31 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("http://localhost:1234/");
-  await expect(page).toHaveTitle("Cribbage Trainer");
+const expectedTitle = "Cribbage Trainer";
+
+test(`has title '${expectedTitle}'`, async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle(expectedTitle);
+});
+
+test("pre-cut hand points show after select of two discards", async ({
+  page,
+}) => {
+  await page.goto("/");
+
+  const discardCount = 2;
+  for (let index = 0; index < discardCount; index += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await page.click(`.hand li:nth-child(${index + 1})`);
+  }
+
+  expect(
+    await Promise.all(
+      (
+        await page.$$("figcaption")
+      ).map(
+        async (figcaption) =>
+          (await figcaption.textContent()) === "Pre-cut hand points:"
+      )
+    ).then((results) => results.some(Boolean))
+  ).toBe(true);
 });
