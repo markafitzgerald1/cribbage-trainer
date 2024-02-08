f9ce8a0d73636be2a3beedde15f67c67d57cf62a
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2024-02-07 22:38:05 -0500

    ✨ feat(privacy policy): create component

---
+++ src/ui-react/PrivacyPolicy.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PrivacyPolicy } from "./PrivacyPolicy";
+
+const meta = {
+  component: PrivacyPolicy,
+  tags: ["autodocs"],
+  title: "PrivacyPolicy",
+} satisfies Meta<typeof PrivacyPolicy>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Policy: Story = {};
