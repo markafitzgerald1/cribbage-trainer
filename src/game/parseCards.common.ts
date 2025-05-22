61818b09dc61a999781072e04349130b3670280b
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2025-05-21 20:18:02 -0400

    🔧 fix: update coverage & parseCards error checks
    Refactor parseCards to throw on invalid card rank input.
    Update coverage configuration per review

--- src/game/parseCards.common.ts
+++ src/game/parseCards.common.ts
@@ -1,7 +1,20 @@
 import { CARD_LABELS, Card, CARDS as card } from "./Card";
 
-export const parseCards = (keepSpecifier: string): Card[] =>
-  keepSpecifier.length === 0
-    ? []
-    : // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      keepSpecifier.split(",").map((rank) => card[CARD_LABELS.indexOf(rank)]!);
+const NOT_FOUND_INDEX = -1;
+
+export const parseCards = (keepSpecifier: string): Card[] => {
+  if (keepSpecifier.length === 0) {
+    return [];
+  }
+
+  return keepSpecifier.split(",").map((rank) => {
+    const cardIndex = CARD_LABELS.indexOf(rank);
+    if (cardIndex === NOT_FOUND_INDEX) {
+      throw new Error(
+        `Invalid card rank encountered in test specifier: '${rank}'`,
+      );
+    }
+    // eslint-disable-next-line security/detect-object-injection, @typescript-eslint/no-non-null-assertion
+    return card[cardIndex]!;
+  });
+};
