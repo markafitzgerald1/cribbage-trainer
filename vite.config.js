e857fdbf9a3d5006302b9630f99771ab7c47c71d
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2025-12-29 02:00:43 -0330

    chore: update storybook docs links

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+// More info at: https://storybook.js.org/docs/writing-tests/integrations/vitest-addon
 export default {
   base: "/cribbage-trainer",
   build: {
@@ -29,7 +29,7 @@ export default {
         extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          // See options at: https://storybook.js.org/docs/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
             configDir: path.join(dirname, ".storybook"),
           }),
