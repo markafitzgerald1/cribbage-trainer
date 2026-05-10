f8608949fb989e83cd56f50e699b3341104ea2e2
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2026-05-10 00:34:19 -0400

    fix(ui): resolve mobile portrait grid layout and prevent horizontal overflow

--- tests-e2e/index.spec.ts
+++ tests-e2e/index.spec.ts
@@ -17,13 +17,12 @@ test(`HTML charset '${expectedCharset}' is specified`, async ({ page }) => {
   expect(await page.$(`meta[charset="${expectedCharset}"]`)).not.toBeNull();
 });
 
-test("double tap zoom disabled to speed up mobile onclick handling", async ({
-  page,
-}) => {
+test("standard mobile viewport is specified", async ({ page }) => {
   await page.goto("/");
-  expect(
-    await page.$('meta[name="viewport"][content="width=device-width"]'),
-  ).not.toBeNull();
+  await expect(page.locator('meta[name="viewport"]')).toHaveAttribute(
+    "content",
+    "width=device-width, initial-scale=1",
+  );
 });
 
 const expectedTitle = "Cribbage Trainer";
